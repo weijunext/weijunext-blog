1fb8e7cf3cfb001aecdbaf8346f8097218d11c54
WEI JUN <weijunext@gmail.com>
2024-01-29 11:34:05 +0800

    style: update footer

--- themes/weijunext/components/Footer.js
+++ themes/weijunext/components/Footer.js
@@ -1,8 +1,6 @@
 import BLOG from '@/blog.config'
-import Image from 'next/image'
 import Link from 'next/link'
-import CONFIG from '../config'
-// import DarkModeButton from '@/components/DarkModeButton'
+import SocialIcon from './social-icons'
 
 const Footer = ({ title }) => {
   const d = new Date()
@@ -15,164 +13,57 @@ const Footer = ({ title }) => {
   })()
 
   return (
-    <footer className="w-full border-t mt-10 flex sm:flex-row flex-col justify-between px-12 py-10 sm:mb-0 mb-3 text-sm bg-black text-gray-100">
-      <div className="">
-        <Link
-          href="/"
-          aria-current="page"
-          className="inline-flex items-center gap-2 text-lg text-secondary-700  md:text-xl"
-        >
-          <Image
-            src="/avatar.png"
-            alt="logo"
-            width={30}
-            height={30}
-            className="h-6 w-6"
+    <footer>
+      <div className="mt-16 pt-6 pb-2 flex flex-col items-center bg-black text-sm text-gray-400 border-t">
+        <div className="mb-3 flex space-x-4">
+          <SocialIcon kind="mail" href="mailto:weijunext@gmail.com" size={6} />
+          <SocialIcon
+            kind="github"
+            href="https://github.com/weijunext"
+            size={6}
           />
-          J实验室
-        </Link>
-        <p className="mt-2">{CONFIG.DESCRIPTION}</p>
-        <p className="mt-2">
-          <i className="fas fa-copyright" /> {`${copyrightDate}`}{' '}
-          <span>
-            <a
-              href={BLOG.LINK}
-              className="underline font-bold  dark:text-gray-300 "
-            >
-              {BLOG.AUTHOR}
-            </a>
-          </span>
-        </p>
-        <p className="mt-2">
-          <span className="hidden busuanzi_container_site_pv">
-            <i className="fas fa-eye" />
-            <span className="px-1 busuanzi_value_site_pv"> </span>{' '}
-          </span>
-          <span className="pl-2 hidden busuanzi_container_site_uv">
-            <i className="fas fa-users" />{' '}
-            <span className="px-1 busuanzi_value_site_uv"> </span>{' '}
-          </span>
-        </p>
-        <p className="mt-2">
-          {BLOG.BEI_AN && (
-            <>
-              <i className="fas fa-shield-alt" />{' '}
-              <a href="https://beian.miit.gov.cn/" className="mr-2">
-                {BLOG.BEI_AN}
-              </a>
-              <br />
-            </>
-          )}
-        </p>
-      </div>
-      <div className="mt-10 grid grid-cols-2 gap-10 text-left md:mt-0 md:flex md:gap-24">
-        <div>
-          <h3 className="font-semibold text-secondary-700">Follow Me</h3>{' '}
-          <ul className="mt-4 space-y-2">
-            <li>
-              <a
-                href="mailto:weijunext@gmail.com"
-                className="hover:text-primary-500 hover:underline"
-              >
-                Email
-              </a>
-            </li>{' '}
-            <li>
-              <Link
-                href="https://twitter.com/weijunext"
-                target="_black"
-                rel="noopener norefferer nofollow"
-                className="hover:text-primary-500 hover:underline"
-              >
-                Twitter / X
-              </Link>
-            </li>{' '}
-            <li>
-              <Link
-                href="https://github.com/weijunext"
-                target="_black"
-                rel="noopener norefferer nofollow"
-                className="hover:text-primary-500 hover:underline"
-              >
-                Github
-              </Link>
-            </li>{' '}
-            <li>
-              <Link
-                href="https://mp.weixin.qq.com/s/RXpu-Ck13zoHyLP1OOUZ-g"
-                target="_black"
-                rel="noopener norefferer nofollow"
-                className="hover:text-primary-500 hover:underline"
-              >
-                公众号
-              </Link>
-            </li>{' '}
-            <li>
-              <Link
-                href="https://juejin.cn/user/26044008768029"
-                target="_black"
-                rel="noopener norefferer nofollow"
-                className="hover:text-primary-500 hover:underline"
-              >
-                掘金
-              </Link>
-            </li>{' '}
-            <li>
-              <Link
-                href="https://m.okjike.com/users/13EF1128-B51B-4D22-8B95-16BB406529F0"
-                target="_black"
-                rel="noopener norefferer nofollow"
-                className="hover:text-primary-500 hover:underline"
-              >
-                即刻
-              </Link>
-            </li>{' '}
-          </ul>
-        </div>{' '}
-        <div>
-          <h3 className="font-semibold text-secondary-700">Links</h3>{' '}
-          <ul className="mt-4 space-y-2">
-            <li>
-              <Link
-                href="https://weijunext.com/"
-                title="Wei Jun's blog, share knowledge about front-end, fullstack and AI"
-                target="_blank"
-                className="hover:text-primary-500 hover:underline"
-              >
-                J 实验室
-              </Link>
-            </li>{' '}
-            <li>
-              <Link
-                href="https://smartexcel.cc/"
-                title="Your Smart Excel formula bot, generating formulas instantly."
-                target="_black"
-                className="hover:text-primary-500 hover:underline"
-              >
-                Smart Excel（开源）
-              </Link>
-            </li>{' '}
-            <li>
-              <Link
-                href="https://nextjs.weijunext.com/"
-                title="Sharing Technologies in the Next.js Ecosystem"
-                target="_black"
-                className="hover:text-primary-500 hover:underline"
-              >
-                Next.js 实战分享
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="https://weijunext.com/make-a-friend"
-                title="Make a friend"
-                target="_blank"
-                className="hover:text-primary-500 hover:underline"
-              >
-                关于我
-              </Link>
-            </li>{' '}
-          </ul>
+          <SocialIcon
+            kind="twitter"
+            href="https://twitter.com/BigYeChengPu"
+            size={6}
+          />
+          <SocialIcon
+            kind="twitterX"
+            href="https://twitter.com/weijunext"
+            size={6}
+          />
+          <SocialIcon
+            kind="jueJin"
+            href="https://juejin.cn/user/26044008768029"
+            size={6}
+          />
+          <SocialIcon
+            kind="weChat"
+            href="https://weijunext.com/make-a-friend"
+            size={6}
+          />
+        </div>
+        <div className="mb-2 flex space-x-2">
+          <Link href="https://weijunext.com/" target="_blank">
+            J实验室
+          </Link>
+          <div>{' • '}</div>
+          <Link href="https://smartexcel.cc/" target="_blank">
+            Smart Excel（开源）
+          </Link>
+          <div>{' • '}</div>
+          <Link href="https://nextjs.weijunext.com/" target="_blank">
+            Next.js 实战分享
+          </Link>
+        </div>
+        <div className="mb-2 flex space-x-2">
+          <Link href={BLOG.LINK}>{BLOG.AUTHOR}</Link>
+          <div>{' • '}</div>
+          <div>{`© ${copyrightDate}`}</div>
+          <div>{' • '}</div>
+          <Link href="https://beian.miit.gov.cn/" target="_blank">
+            <i className="fas fa-shield-alt" /> {BLOG.BEI_AN}
+          </Link>
         </div>
       </div>
     </footer>
